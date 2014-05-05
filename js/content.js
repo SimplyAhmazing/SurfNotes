eb6672288644695ff90a5d9462aa285f5fb29232
Ahmed Abdalla <aabdalla@amplify-nation.com>
2014-05-04 23:19:39 -0400

    can save notes

--- js/content.js
+++ js/content.js
@@ -1,31 +1,30 @@
 var HTML_TEMPLATES = null;
 
 function buildNote(top, right, bottom, left){
-    var note_top = top || '0px';
-    var note_right = right || '0px';
-    var note_bottom = bottom || '0px';
-    var note_left = left || '0px';
+    // var note_top = top || '0px';
+    // var note_right = right || '0px';
+    // var note_bottom = bottom || '0px';
+    // var note_left = left || '0px';
 
-    var z_index = 1000;
-
-    // var $noteDiv = $('<div>');
-    // $noteDiv.addClass('note');
-    //
+    var noteUUID = guid();
+    var tabUrl = window.location.href;
+    var newTemplateContext = {
+        created_at: new Date(),
+        site: tabUrl,
+        uuid: noteUUID
+    };
     var templateObj = Handlebars.compile(HTML_TEMPLATES.note);
-    var template = templateObj({});
-
+    var template = templateObj(newTemplateContext);
     $noteDiv = $(template);
-
     $noteDiv.resizable();
     $noteDiv.draggable();
-
-
-    // Position the note
-    // $noteDiv.css('top', note_top);
-    // $noteDiv.css('right', note_right);
     $noteDiv.css('position', 'fixed');
-    // Add uuid
-    $noteDiv.data('uuid', guid());
+    $noteDiv.addClass(noteUUID);
+    $noteDiv.data('uuid', noteUUID);
+
+    attachSaveListener($noteDiv, '#save-note-' + noteUUID);
+    attachAutoSaveListener($noteDiv, '#notearea-' + noteUUID);
+    attachDeleteListener($noteDiv, '#delete-note-' + noteUUID);
 
     return $noteDiv;
 }
@@ -56,6 +55,62 @@ chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
     }
 });
 
+var DB = new function(){
+    this.save = function(noteObj){
+        this._send({type: 'save-note', body: noteObj});
+    };
+    this.remove = function(noteUUID){
+        this._send({type: 'remove-note', body: noteUUID});
+    };
+    this.load = function(noteUUID){
+        this._send({type: 'load-note', body: noteUUID});
+    };
+    this.filter = function(query){
+        this._send({type: 'load-notes', body: query});
+    };
+    this._send = function(msg, respCallBack){
+        chrome.runtime.sendMessage(msg);
+    };
+};
+
+function attachAutoSaveListener($el, buttonQuerySelecter){
+    $(buttonQuerySelecter, $el).on('change', function(e){
+        e.preventDefault();
+        var $form = $(this).parent();
+        var noteObj = $form.serializeObject();
+        var noteUUID = $form.data('uuid');
+        var saveMsg = {uuid: noteUUID, note: noteObj};
+        console.log('Auto-saving note: ', noteObj);
+        DB.save(saveMsg);
+        return false;
+    });
+}
+
+function attachSaveListener($el, buttonQuerySelecter){
+    $(buttonQuerySelecter, $el).on('click', function(e){
+        e.preventDefault();
+        var $form = $(this).parent();
+        var noteObj = $form.serializeObject();
+        var noteUUID = $form.data('uuid');
+        var saveMsg = {uuid: noteUUID, note: noteObj};
+        console.log('Save note: ', saveMsg);
+        DB.save(saveMsg);
+        $form.remove();
+        return false;
+    });
+}
+
+function attachDeleteListener($el, buttonQuerySelecter){
+    $(buttonQuerySelecter, $el).on('click', function(e){
+        e.preventDefault();
+        var noteUUID = $(this).parent().data('uuid');
+        console.log('Deleted this note: ', noteUUID);
+        DB.remove(noteUUID);
+        $(this).parent().remove();
+        return false;
+    });
+}
+
 function guid() {
     /* Returns a random uuid */
     function s4() {
@@ -66,3 +121,22 @@ function guid() {
     return s4() + s4() + '-' + s4() + '-' + s4() + '-' +
         s4() + '-' + s4() + s4() + s4();
 }
+
+$.fn.serializeObject = function()
+{
+    // Src:http://stackoverflow.com/questions/1184624/
+    // convert-form-data-to-js-object-with-jquery
+    var o = {};
+    var a = this.serializeArray();
+    $.each(a, function() {
+        if (o[this.name] !== undefined) {
+            if (!o[this.name].push) {
+                o[this.name] = [o[this.name]];
+            }
+            o[this.name].push(this.value || '');
+        } else {
+            o[this.name] = this.value || '';
+        }
+    });
+    return o;
+};
--- js/index.js
+++ js/index.js
@@ -1,18 +1,49 @@
-// function createNewNote(){
-// }
-// 
-// function toggleNoteVisibility(){
-// }
-// chrome.tabs.executescript(
-//             null,{code:"alert('meeeeeeh');"}
-//         );
-// chrome.extension.onMessage.addListener(function(message, sender, sendResponse) {
-//     switch(message.action) {
-//         case "new-note":
-//             createNewNote();
-//             break;
-//         case "toggle-visibility":
-//             toggleNoteVisibility();
-//             break;
-//     }
-// });
+var DB = {
+    saveNote: function(data){
+        var note = JSON.stringify(data.note);
+        window.localStorage.setItem(data.uuid, note);
+    },
+    // this.loadNote = function (noteUUID){
+    //     return window.localStorage.getItem(noteUUID);
+    // };
+    // this.removeNote = function (noteUUID){
+    //     return window.localStorage.removeItem(noteUUID);
+    // };
+
+    // this.listNotes = function (){
+    //     //iterate through local storage
+    // };
+};
+
+
+
+chrome.runtime.onMessage.addListener(function(request, sender, sendResponse){
+    // Save Messages
+    // Load saved messages
+    //
+    switch(request.type){
+        case "save-note":
+            debugger;
+            DB.saveNote(request.body);
+            //alert('saving a note..');
+            break;
+
+        case "remove-note":
+            resp = DB.removeNote(request.body);
+            alert('saving a note..');
+            break;
+
+        case "load-note":
+            resp = DB.loadNote(request.body);
+            sendResponse(resp);
+            alert('saving a note..');
+            break;
+
+        case "list-notes":
+            resp = DB.loadNote(request.body);
+            sendResponse(resp);
+            alert('saving a note..');
+            break;
+    }
+});
+
