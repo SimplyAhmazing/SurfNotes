5226e94bce7dc5848f60a2c10cbf8506db39e654
Ahmed Abdalla <aabdalla@amplify-nation.com>
2014-11-09 14:40:55 -0500

    card filter based on site

--- js/dashboard.js
+++ js/dashboard.js
@@ -50,23 +50,38 @@ var surfNotes = angular.module('surfNotes', [
 angular.module('surfNotes.controllers', [])
     .controller('notesController', notesController);
 
-function notesController($scope){
+function notesController($scope, $filter){
     function init(){
         $scope.notes = [];
+        $scope.noteSites = [];
         for (var i in window.localStorage){
             try{
-                $scope.notes.push(
-                    JSON.parse(window.localStorage.getItem(i))
-                );
+                var note = JSON.parse(window.localStorage.getItem(i));
+                note.hostname = $filter('getHostname')(note.url);
+                $scope.notes.push(note);
             }catch (e){
                 console.log("Error reading note", window.localStorage.getItem(i));
                 console.log("Error is", err);
             }
         }
+
+        var sitesList = [];
+        $scope.noteSites = $scope.notes.filter(function(note){
+            var parser = document.createElement('a');
+            parser.href = note.url;
+            if (sitesList.indexOf(parser.hostname) === -1){
+                sitesList.push(parser.hostname);
+                return true;
+            }
+            return false;
+        });
+
         $scope.delete = function(uuid){
             console.log('Deleting note:', uuid);
             window.localStorage.removeItem(uuid);
-            $scope.$apply();
+            $scope.notes.filter(function(note){
+                return note.uuid != uuid;
+            });
         };
     };
     init();
@@ -79,11 +94,25 @@ angular.module('surfNotes.directives', []).
             templateUrl: chrome.extension.getURL("html/templates/list-notes.html")
         }
     })
+    .directive("listSites", function(){
+        return {
+            restrict: "EA",
+            replace: true,
+            templateUrl: chrome.extension.getURL("html/templates/list-sites.html")
+        }
+    })
 
 angular.module('surfNotes.filters', [])
     .filter("asDate", function () {
         return function (input) {
             return new Date(input);
+        }
+    })
+    .filter("getHostname", function () {
+        return function (url) {
+            var parser = document.createElement('a');
+            parser.href = url;
+            return parser.hostname;
         }
     });
 
