44080a951f72c8def245a9c617d5116a1c3265ae
Ahmed Abdalla <aabdalla@amplify-nation.com>
2014-11-09 23:01:22 -0500

    fixes updating on delete

--- js/dashboard.js
+++ js/dashboard.js
@@ -1,47 +1,3 @@
-// $(document).ready(function(){
-//     for (var i in window.localStorage){
-//         var noteStr = window.localStorage.getItem(i);
-//         var noteObj = JSON.parse(noteStr);
-//         if (noteObj){
-//             $('#notes').append(createNoteHTML(noteObj));
-//         }
-//     }
-// });
-// 
-// 
-// function createNoteHTML(noteObj){
-//     var $container = $('<div>').attr('id', 'note');
-//     var $title = $('<h3>').html(noteObj.title);
-//     var $url = $('<p>')
-//         .html(noteObj.url)
-//         .addClass('note-url');
-//     var $createdAt = $('<div>').html(
-//             "Created at: " + Date(noteObj.created_at) +"."
-//         )
-//         .addClass('created-at');
-//     var $modifiedAt = $('<div>').html(
-//             "Last updated: " + Date(noteObj.modified_at) + "."
-//         )
-//         .addClass('modified-at');
-//     var $noteContent = $('<div>')
-//         .attr('id', 'note-content')
-//         .html(noteObj.note)
-//         .addClass('note-content');
-//     $container.append(
-//         $title,
-//         $createdAt,
-//         $modifiedAt,
-//         $url,
-//         $noteContent
-//     );
-//     var $panel = $('<li>').addClass('panel panel-default');
-//     var $panelBody = $('<div>').addClass('panel-body');
-//     var $panelInfo = $('<div>').addClass('panel-info');
-//     $panel.append($panelBody.append($panelInfo.append($container)))
-//     $panel.attr('id', noteObj.uuid);
-//     return  $panel;
-// }
-//
 var surfNotes = angular.module('surfNotes', [
     'surfNotes.filters',
     'surfNotes.directives',
@@ -78,16 +34,15 @@ function notesController($scope, $filter, $sce){
             }
             return false;
         });
-
-        $scope.delete = function(uuid){
-            console.log('Deleting note:', uuid);
-            window.localStorage.removeItem(uuid);
-            $scope.notes.filter(function(note){
-                return note.uuid != uuid;
-            });
-        };
     };
     init();
+    $scope.delete = function(uuid){
+        console.log('Deleting note:', uuid);
+        window.localStorage.removeItem(uuid);
+        $scope.notes = $scope.notes.filter(function(note){
+            return note.uuid != uuid;
+        });
+    };
 }
 angular.module('surfNotes.directives', []).
     directive("listNotes", function(){
